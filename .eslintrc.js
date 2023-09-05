b772049118ca295180cbeb2391a2c1d0ed70ebdc
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-09-05 12:09:55 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,36 @@ module.exports = {
     env: {
         browser: false,
         es6: true,
-        jest: true,
-    },
+        jest/ globals: true,
+},
     extends: [
-        'airbnb-base',
-        'plugin:jest/all',
-    ],
+    'airbnb-base',
+    'plugin:jest/all',
+],
     globals: {
-        Atomics: 'readonly',
+    Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
     },
-    parserOptions: {
-        ecmaVersion: 2018,
+parserOptions: {
+    ecmaVersion: 2018,
         sourceType: 'module',
     },
-    plugins: ['jest'],
+plugins: ['jest'],
     rules: {
-        'max-classes-per-file': 'off',
+    'max-classes-per-file': 'off',
         'no-underscore-dangle': 'off',
-        'no-console': 'off',
-        'no-shadow': 'off',
-        'no-restricted-syntax': [
-            'error',
-            'LabeledStatement',
-            'WithStatement',
-        ],
+            'no-console': 'off',
+                'no-shadow': 'off',
+                    'no-restricted-syntax': [
+                        'error',
+                        'LabeledStatement',
+                        'WithStatement',
+                    ],
     },
-    overrides: [
-        {
-            files: ['*.js'],
-            excludedFiles: 'babel.config.js',
-        }
-    ]
+overrides: [
+    {
+        files: ['*.js'],
+        excludedFiles: ['babel.config.js', '.eslintrc.js'],
+    }
+]
 };
